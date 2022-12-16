029a142730d2e554df1dae416e213d18aca5ce8c
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-12-16 10:09:32 -0400

    ajeitando midias

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const projetos = [
         "titulo": "Atividade 01",
         "descricao": "Nesta atividade você deverá criar uma página HTML simples com o plano de ensino da disciplina.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/Atividades/Atividade01",
-        "video": "src/videos/atividade01.mp4",
+        "video": "src/videos/atividade01.gif",
         "badges": [
             "html", "css"
         ]
@@ -12,7 +12,7 @@ const projetos = [
         "titulo": "Atividade 02",
         "descricao": "Nesta atividade, você deverá tratar o evento de saída do mouse do quadrado de forma a restaurar a cor original usando JavaScript",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/Atividades/Atividade02",
-        "video": "src/videos/atividade02.mp4",
+        "video": "src/videos/atividade02.gif",
         "badges": [
             "html", "css"
         ]
@@ -21,7 +21,7 @@ const projetos = [
         "titulo": "Atividade 03",
         "descricao": "Nesta atividade devesse implementar um paint com base no código dado. Ferramentas que devem ser adicionadas: circulo, balde de tinta e texto.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/Atividades/Atividade03",
-        "video": "src/videos/atividade03.mp4",
+        "video": "src/videos/atividade03.gif",
         "badges": [
             "html", "css","js"
         ]
@@ -30,7 +30,7 @@ const projetos = [
         "titulo": "Atividade 04",
         "descricao": "Nesta atividade você deverá modificar o arquivo modelo disponibilizado para atualizá-lo com seus dados. Além de colocar seus dados, você deverá inserir a informação de quando a página foi gerada pelo servidor, fazendo uso de PHP para isso.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/Atividades/Atividade04",
-        "video": "src/videos/atividade04.mp4",
+        "video": "src/videos/atividade04.gif",
         "badges": [
             "html", "css", "php"
         ]
@@ -39,7 +39,7 @@ const projetos = [
         "titulo": "Atividade 05",
         "descricao": "Nesta atividade vocês deverão ordernar um conjunto de palavras provido pelo usuário. Para isso já está pronta uma base e vocês devem simplesmente implementar a lógica de ordenação das palavras.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/Atividades/Atividade05",
-        "video": "src/videos/atividade05.mp4",
+        "video": "src/videos/atividade05.gif",
         "badges": [
             "html", "css", "php"
         ]
@@ -48,7 +48,7 @@ const projetos = [
         "titulo": "Prova 01 - Exercicio 01",
         "descricao": "Crie um pequena página usando javascript e html que converte Real para Dólar.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova01/exercicio01",
-        "video": "src/videos/prova01_exercicio01.mp4",
+        "video": "src/videos/prova01_exercicio01.gif",
         "badges": [
             "html", "css", "js"
         ]
@@ -57,7 +57,7 @@ const projetos = [
         "titulo": "Prova 01 - Exercicio 02",
         "descricao": "Crie uma página e com um retângulo e usando javascript faça que a cada click do mouse o quadrado mude de cor, na seguinte ordem vermelho, azul, verde, rosa, amarelo e então volte para vermelho.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova01/exercicio02",
-        "video": "src/videos/prova01_exercicio02.mp4",
+        "video": "src/videos/prova01_exercicio02.gif",
         "badges": [
             "html", "css", "js"
         ]
@@ -66,7 +66,7 @@ const projetos = [
         "titulo": "Prova 01 - Exercicio 03",
         "descricao": "Sua página deve ter um botão calcular que deve exibir a lista ordenada de maneira decrescente pela data, exibir o produto mais vendido, o produto mais caro, o produto com maior faturamento.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova01/exercicio03",
-        "video": "src/videos/prova01_exercicio03.mp4",
+        "video": "src/videos/prova01_exercicio03.gif",
         "badges": [
             "html", "css", "js"
         ]
@@ -75,7 +75,7 @@ const projetos = [
         "titulo": "Prova 01 - Exercicio 04",
         "descricao": "Crie uma calculadora usando javascript, sua calculadora deve ser capaz de realizar as seguintes operações: multiplicação, divisão, soma, subtração, fatorial, raiz quadrada e exponenciação.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova01/exercicio04",
-        "video": "src/videos/prova01_exercicio04.mp4",
+        "video": "src/videos/prova01_exercicio04.gif",
         "badges": [
             "html", "css", "js"
         ]
@@ -84,7 +84,7 @@ const projetos = [
         "titulo": "Prova 02 - Exercicio 01",
         "descricao": "Crie uma API rest usando php, sua api deve entregar as cotações de cada uma das moedas listadas no arquivo disponibilizado, baixe o arquivo e o use no seu projeto para as consultas da API.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova02/exercicio01",
-        "video": "src/videos/prova02_exercicio01.mp4",
+        "video": "src/videos/prova02_exercicio01.gif",
         "badges": [
             "html", "css", "php"
         ]
@@ -93,7 +93,7 @@ const projetos = [
         "titulo": "Prova 02 - Exercicio 02",
         "descricao": "Crie uma calculadora usando javascript e php, sua calculadora deve ser capaz de realizar as seguintes operações: multiplicação, divisão, soma, subtração, fatorial, raiz quadrada e exponenciação. As operações devem ser computadas do lado do servidor.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova02/exercicio02",
-        "video": "src/videos/prova02_exercicio02.mp4",
+        "video": "src/videos/prova02_exercicio02.gif",
         "badges": [
             "html", "css", "js", "php"
         ]
@@ -102,7 +102,7 @@ const projetos = [
         "titulo": "Prova 02 - Exercicio 03",
         "descricao": "Crie uma página em php que tenha um formulário com os seguintes campos: nome, sobrenome, cep, telefone, data de nascimento. Seu programa deve guardar esses dados em um arquivo .csv.",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova02/exercicio03",
-        "video": "src/videos/prova02_exercicio03.mp4",
+        "video": "src/videos/prova02_exercicio03.gif",
         "badges": [
             "html", "css", "php"
         ]
@@ -111,7 +111,7 @@ const projetos = [
         "titulo": "Prova 02 - Exercicio 04",
         "descricao": "Crie um programa em PHP que exibe os dados guardados em um arquivo .csv (exercicio 03).",
         "git": "https://github.com/camilavitoriacosta/ProgWeb/tree/master/prova02/exercicio04",
-        "video": "src/videos/prova02_exercicio04.mp4",
+        "video": "src/videos/prova02_exercicio04.gif",
         "badges": [
             "html", "css", "php"
         ]
@@ -128,7 +128,7 @@ projetos.forEach(projeto => {
 
 function criarCard(projeto) {
     let card = "<div class='card-projeto'>"
-    let video = "<video autoplay loop src='" + projeto.video + "' class='card-projeto__video'></video>"
+    let video = "<img  alt='video projeto' src='" + projeto.video + "' class='card-projeto__video'></img>"
     let tecnologias = "<ul class='card-projeto__tecnologias'>"
 
     projeto.badges.forEach(badge => {
